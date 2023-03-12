3071d6af637de2fff26850e4651424c66ad870c3
0riioncode <121803202+0riioncode@users.noreply.github.com>
2023-03-12 10:17:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-window.location.href = "https://www.youtube.com/watch?v=Krcr3_oNcts&list=PLdkm7zcTKfQcZCfuEowZiOuqpFpDSNBv5&index=5";
+window.location.href = "https://www.youtube.com/watch?v=9LIOrVmSFR4&list=PLdkm7zcTKfQcZCfuEowZiOuqpFpDSNBv5&index=15";
