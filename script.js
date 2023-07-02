b93ce5ba70a97653339076f4642062d44d9c1fb8
0riioncode <121803202+0riioncode@users.noreply.github.com>
2023-07-01 20:49:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-window.location.href = "https://www.youtube.com/watch?v=V8FrneqVXl8&list=PLdkm7zcTKfQfRA8jZYgqLbB6P7SYsRwSb";
+window.location.href = "https://www.youtube.com/watch?v=pCmylg58mnU&list=PLdkm7zcTKfQfRA8jZYgqLbB6P7SYsRwSb&index=15&ab_channel=CasoCerrado";
